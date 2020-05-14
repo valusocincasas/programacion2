5557488fdfee97777bcc3ea12fe8f763fd89599b
serena <spapazian@udesa.edu.ar>
2020-05-14 19:26:53 -0300

    cambio en package.json

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var buscadorRouter = require('./routes/buscador')
+var buscadorRouter = require('./routes/buscador');
 
 var app = express();
 
