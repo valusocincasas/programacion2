d7216c65b92b3239739e4af5a42679e16509741f
serena <spapazian@udesa.edu.ar>
2020-06-02 21:48:04 -0300

    modelo de resena creado y vistas de resenas

---
+++ database/models/resenaModels.js
@@ -0,0 +1,26 @@
+module.exports = function (sequelize, DataTypes) {
+const Resena = sequelize.define (
+    'Resena',
+    { 
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true, 
+            autoIncrement: true 
+        },
+        peliculaId: DataTypes.INTEGER,
+        usuarioId: DataTypes.INTEGER,
+        textoResena: DataTypes.STRING,
+        puntaje: DataTypes.INTEGER,
+    }, 
+    {
+        tableName: "resenas"
+    }
+)
+Resena.associate = function (models) {
+    Resena.belongsTo (models.usuario, {
+        as: 'usuario', 
+    foreignKey: 'usuarioId'
+    })
+}
+ }
+ return Resena
